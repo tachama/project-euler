8c0f0c40e2850eb7b96eb66f5034d352b8bac36b
tachama <tac.hama@gmail.com>
2012-01-21 14:29:04 +0900

    customize for execute speed.

--- pr010.py
+++ pr010.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 #
-# XXX - this needs too many run-time...
-#
 
 '''
 Problem 10
@@ -13,28 +11,25 @@ Find the sum of all the primes below two million.
 
 INPUT_NUM = 2000000
 
-def eratoshenes(lst):
-  '''eratoshenes algorithm for generate prime'''
-  idx = 0
-  length = len(lst)
-  while idx < length:
-    n = lst[idx]
-    for i in range(length - 1, idx, -1):
-      if lst[i] % n == 0:
-        lst.pop(i)   # is faster than 'del lst[i]' ?
+def eratoshenes(num):
+  '''eratoshenes algorithm for prime numbers'''
+  num_lst = range(num)   # list of all number; [0,1,2...]
+  prm_lst = [True for i in num_lst]  # number in num_lst is prime ?
+  idx = 2
+  while idx < num:
+    if prm_lst[idx]:  # check number is marked as prime ?
+      # mark all multiples as NOT prime
+      idx2 = 2 * idx
+      while idx2 < num:
+        prm_lst[idx2] = False
+        idx2 += idx
     idx += 1
-    length = len(lst)
-
-def prime(num):
-  '''create prime list by Eratoshenes'''
-  lst = range(3,num,2)  # prepare only odd number (>= 3)
-  eratoshenes(lst)
-  lst.insert(0, 2)  # insert '2' to list head
-  return lst
+  # return number that marked prime. except 0 and 1.
+  return [n for b, n in zip(prm_lst, num_lst)[2:] if b]
 
 def main(num):
   '''main function'''
-  print sum(prime(num))
+  print sum(eratoshenes(num))
 
 if __name__ == '__main__':
   main(INPUT_NUM)
