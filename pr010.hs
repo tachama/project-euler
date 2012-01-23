62a1737c29f35d8e51d41c31ac5f71146d0f1a7c
tachama <tac.hama@gmail.com>
2012-01-23 22:27:23 +0900

    change comment. this program is very slow...

--- pr010.hs
+++ pr010.hs
@@ -5,12 +5,20 @@ The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
 Find the sum of all the primes below two million.
 -}
 
+{-
+ XXX
+ XXX This program execute very slow.
+ XXX needs about 1 hour and 30 minites at Intel Core-I5 2.53GHz
+ XXX
+-}
+
 module Main where
 
 input_number :: Int
 input_number = 2000000
 
--- XXX: maybe waste stack
+
+-- XXX: this function very slow at deep recursive...
 eratoshenes :: [Int] -> [Int]
 eratoshenes [] = []
 eratoshenes (n:ns) = n: eratoshenes (filter (\x -> x `mod` n /= 0) ns)
