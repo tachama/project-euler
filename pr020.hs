c17f59ca1b9ca0c21c4fb10491504f8a8b4e8b8e
tachama <tac.hama@gmail.com>
2012-03-20 12:03:30 +0900

    initial commit

---
+++ pr020.hs
@@ -0,0 +1,34 @@
+{-
+n! means n *(n  1) * ... * 3 * 2 * 1
+
+For example, 10! = 10 * 9 * ... * 3 * 2 * 1 = 3628800,
+and the sum of the digits in the number 10! is 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
+
+Find the sum of the digits in the number 100!
+-}
+
+module Main where
+
+import List (unfoldr)
+
+input_num = 100  -- problem input number.
+
+-- factorial of number
+factorial :: Integer -> Integer
+factorial 1 = 1
+factorial (n+1) = (n+1) * factorial n
+
+-- sum of digits
+digit_sum :: Integer -> Integer
+digit_sum n = sum $ unfoldr divide n
+
+-- subroutine for digit_sum. using by unfoldr.
+divide :: Integer -> Maybe (Integer, Integer)
+divide 0 = Nothing
+divide n = Just (n `mod` 10, n `div` 10)
+
+-- solve problem
+solve :: Integer -> Integer
+solve n = digit_sum $ factorial n
+
+main = print $ solve input_num
