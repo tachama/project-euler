4dc99743091dfc03e21b1266577875fce08ebe02
tachama <tac.hama@gmail.com>
2012-03-28 10:31:25 +0900

    fix problem comment

--- pr022.py
+++ pr022.py
@@ -9,7 +9,7 @@ value by its alphabetical position in the list to obtain a name score.
 
 For example, when the list is sorted into alphabetical order, COLIN, which is
 worth 3 + 15 + 12 + 9 + 14 = 53, is the 938th name in the list. So, COLIN
-would obtain a score of 938  53 = 49714.
+would obtain a score of 938 * 53 = 49714.
 
 What is the total of all the name scores in the file?
 """
