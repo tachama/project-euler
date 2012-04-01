a07828ecb72c97ebf9705809eeb03f567a10abb1
tachama <tac.hama@gmail.com>
2012-04-01 14:13:35 +0900

    performance tuning

--- pr019.hs
+++ pr019.hs
@@ -18,60 +18,63 @@ How many Sundays fell on the first of the month during the twentieth century
 
 module Main where
 
--- day
+-- data definition for DAY
 data Day = SUNDAY | MONDAY | TUESDAY | WEDNESDAY | THURSDAY | FRIDAY | SATURDAY
            deriving (Eq,Show)
 
--- day calculator
-step_day :: Day -> Int -> Day
-step_day day 0 = day
-step_day day (n+1) = step_day (next_day day) n
+-- 1900 is base year of this problem.
+baseYear :: Int
+baseYear = 1900
 
-next_day :: Day -> Day
-next_day day = case day of
-                 SUNDAY -> MONDAY
-                 MONDAY -> TUESDAY
-                 TUESDAY -> WEDNESDAY
-                 WEDNESDAY -> THURSDAY
-                 THURSDAY -> FRIDAY
-                 FRIDAY -> SATURDAY
-                 SATURDAY -> SUNDAY
+-- year is leap?
+isLeap :: Int -> Bool
+isLeap n = (n `mod` 4 == 0) && ((n `mod` 100 /= 0) || (n `mod` 400 == 0))
 
--- check year is leap?
-is_leap :: Int -> Bool
-is_leap y = (y `mod` 4 == 0) && ((y `mod` 100 /= 0) || (y `mod` 400 == 0))
+-- day calculators
+daysLst = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]  -- non-leap
+daysLstL = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]  -- leap
 
--- how many days in each month.
-days_lst      = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-days_lst_leap = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+isSunday :: Day -> Bool
+isSunday d = d == SUNDAY
 
--- number of days at given year -- XXX: can't be defined as list?
-nday_in_year :: Int -> Int
-nday_in_year year = if is_leap year
-                    then sum days_lst_leap
-                    else sum days_lst
+stepDay :: Int -> Day -> Day
+stepDay 0 d = d
+stepDay (n+1) d = stepDay (n `mod` 7) (nextDay d)
 
--- first days of each year
-fst_day_year :: Int -> Day
-fst_day_year 1900 = MONDAY
-fst_day_year (n+1) = step_day (fst_day_year n) (nday_in_year n)
+nextDay :: Day -> Day
+nextDay d = case d of
+              SUNDAY -> MONDAY
+              MONDAY -> TUESDAY
+              TUESDAY -> WEDNESDAY
+              WEDNESDAY -> THURSDAY
+              THURSDAY -> FRIDAY
+              FRIDAY -> SATURDAY
+              SATURDAY -> SUNDAY
 
--- return first days list of given year.
-fst_days :: Int -> [Day]
-fst_days n = let st = fst_day_year n
-                 dl = if is_leap n
-                      then days_lst_leap
-                      else days_lst
-             in [step_day st (sum $ take d dl) | d <- [0..11]]
+-- years list of
+yearDays :: Int -> Day -> ([Day], Day)
+yearDays y d = let dl = if isLeap y then daysLstL else daysLst
+                   ds = yearDaysSub d dl
+               in
+                 (init ds, last ds)
 
--- count of the first dai of the month is Sunday.
-num_fst_sunday :: Int -> Int
-num_fst_sunday n = let fds = fst_days n
-                   in
-                     length $ filter (\d -> d == SUNDAY) fds
+yearDaysSub :: Day -> [Int] -> [Day]
+yearDaysSub d [] = [d]
+yearDaysSub d (n:ns) = let d2 = stepDay n d
+                       in
+                         d : (yearDaysSub d2 ns)
 
--- solve problem
+-- fold function for solve.
+f :: [([Day], Day)] -> Int -> [([Day], Day)]
+f ds y = ds ++ [yearDays y (snd (last ds))]
+
+-- solve problem.
 solve :: Int -> Int -> Int
-solve start end = sum $ map num_fst_sunday [start .. end]
+solve start end = let lst = foldl f [yearDays baseYear MONDAY] [(baseYear+1) .. end]
+                      lst2 = drop (start - baseYear) lst
+                      lst3 = map fst lst2
+                  in
+                    sum $ map (length . (filter isSunday)) lst3
 
-main = print $ solve 1901 2000  -- count 1901 to 2000
+-- main function.
+main = print $ solve 1901 2000
