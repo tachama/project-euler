b49bb82dd3248143ce180cec9a3c37cbd29a051b
tachama <tac.hama@gmail.com>
2012-12-14 23:04:24 +0900

    fix problem

--- pr009.py
+++ pr009.py
@@ -7,10 +7,10 @@ Problem 9
 A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
 
 a ** 2 + b ** 2 = c ** 2
-For example, 32 + 42 = 9 + 16 = 25 = 52.
+For example, 3 ** 2 + 4 ** 2 = 9 + 16 = 25 = 52.
 
 There exists exactly one Pythagorean triplet for which a + b + c = 1000.
-Find the product abc.
+Find the product a*b*c.
 '''
 
 INPUT_NUM = 1000
