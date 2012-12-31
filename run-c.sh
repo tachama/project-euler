23536b6a3dbbcd15fa2297bcacec47908e3f1be6
tachama <tac.hama@gmail.com>
2012-12-31 16:42:46 +0900

    clean core file

--- run-c.sh
+++ run-c.sh
@@ -7,12 +7,13 @@ copts="-Wall -Werror"
 
 # a.out file name
 aout=a.out
+core=core
 
 compile_execute() {
     ${cc} ${copts} $1
     if [ $? -eq 0 ]; then
         ./${aout}
-        rm ${aout}
+        rm -f ${aout} ${core}
     fi
 }
 
